8b4abb10b11ef482ded9cdb575407f34f5f55848
Troopy <40128411+troopaloop8@users.noreply.github.com>
2020-05-07 13:28:29 +0900

    mvp done

--- index.js
+++ index.js
@@ -9,25 +9,53 @@ let users = [];
 
 // | POST   | /api/users     | Creates a user using the information sent inside the `request body`.                                   |
 
-server.post('/api/users', (req, res) => {
+server.post("/api/users", (req, res) => {
     const user = req.body;
+    if (!user.name || !user.bio) {
+      res
+        .status(400)
+        .json({ errorMessage: "Please provide name and bio for the user." });
+    }
     user.id = shortid.generate();
     users.push(user);
-    res.status(200).json(users);
-})
+    if (!users.find((newUser) => newUser.id === user.id)) {
+      res.status(500).json({
+        errorMessage: "There was an error while saving the user to the database",
+      });
+    }
+    res.status(201).json({ message: `successfully posted user ${user.name}` });
+  });
+
+
 
 // | GET    | /api/users     | Returns an array users.                                                                                |
 
 server.get('/api/users', (req, res) => {
-    res.status(200).json(users);
-})
+    if (users.length > 0) {
+        res.status(200).json(users);
+    } else if (users.length === 0) {
+        res.status(501).json({errorMessage: "There are no users to be retrieved"})
+    } else {
+        res.status(500).json({errorMessage: "The user info could not be retrieved"})
+    }
+});
 
 // | GET    | /api/users/:id | Returns the user object with the specified `id`.                                                       |
 
 server.get('/api/users/:id', (req, res) => {
     const { id } = req.params;
-    const idIndex = users.findIndex((user) => user.id === id)
-    res.status(200).json(users[idIndex])
+    const idIndex = users.findIndex((user) => user.id === id);
+    if (idIndex === -1) {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      }
+      if (!users[idIndex]) {
+        res
+          .status(500)
+          .json({ message: "The user information could not be retrieved." });
+      }  
+    res.status(200).json(users[idIndex]);
 })
 
 
@@ -37,11 +65,14 @@ server.delete('/api/users/:id', (req, res) => {
     const { id } = req.params;
 
     const found = users.filter(user => user.id === id)
-    if (found) {
+    
+    if (!found) {
+        res.status(404).json({ message: "user not found"})
+    } else if (found) {
         users = users.filter(user => user.id !== id);
         res.status(200).json(found);
-    } else {
-        res.status(404).json({ message: "user not found"})
+    } else  {
+        res.status(500).json({ errorMessage: "The user could not be removed"})
     }
 })
 
@@ -51,7 +82,30 @@ server.delete('/api/users/:id', (req, res) => {
 server.patch('/api/users/:id', (req, res) => {
     const { id } = req.params;
     const changes = req.body;
-    let index = hubs
+    const idIndex = users.findIndex((user) => user.id === id);
+    changes.id = users[idIndex].id;
+    users[idIndex] = changes
+    
+    if (idIndex === -1) {
+        res.status(404).json({errorMessage: "The user with the specified ID does not exist"})
+    }  
+    
+    if (!changes.name || !changes.bio) {
+        res
+          .status(400)
+          .json({ errorMessage: "Please provide name and bio for the user." });
+    }
+
+    if(users[idIndex] === changes) {
+        res.status(200).json(users)
+    } else {
+        res.status(500).json({ errorMessage: "The user information could not be modified!"})
+    }
+
+
+    
+
+
 })
 
 
